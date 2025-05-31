385960433749a1735e8a8a2fbeccb20ae91b187c
Karpo95 <karpoBog@gmail.com>
2025-05-31 14:19:36 +0300

    initial commit

---
+++ AITwin/AITwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  AITwinApp.swift
+//  AITwin
+//
+//  Created by Karpenko Bohdan on 31.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AITwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AITwinTests/AITwinTests.swift
@@ -0,0 +1,17 @@
+//
+//  AITwinTests.swift
+//  AITwinTests
+//
+//  Created by Karpenko Bohdan on 31.05.2025.
+//
+
+import Testing
+@testable import AITwin
+
+struct AITwinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
